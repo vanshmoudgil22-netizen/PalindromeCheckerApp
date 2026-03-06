b40c8da7883976a671a775afefd8b7ca37e4b228
vanshmoudgil22-netizen <vanshmoudgil22@gmail.com>
2026-03-06 22:08:27 +0530

    UC1: Application Entry & Welcome Message

---
+++ App/src/UC1PalindromeChecker.java
@@ -0,0 +1,8 @@
+public class UC1PalindromeChecker {
+    public static void main(String[] args){
+        System.out.println("Welcome to the Palindrome Checker management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System Initialized Successfully!");
+    }
+
+}
